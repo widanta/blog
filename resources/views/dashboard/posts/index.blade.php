6abe4d2acbe03d8ae03bf824a737be1c495295d9
widanta <widanta66@gmail.com>
2021-11-13 09:12:52 +0800

    update dan delete

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -31,8 +31,12 @@
             <td>{{ $post->category->name }}</td>
             <td>
               <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-              <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-              <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+              <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+              <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="badge bg-danger border-0" onclick="return confirm('are you sure?')"><span data-feather="x-circle"></span></button>
+              </form>
             </td>
           </tr>
         @endforeach
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
       <h3 class="mb-4">{{$post->title}}</h3>
 
       <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back </a>
-      <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-      <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+      <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+      <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn bg-danger" onclick="return confirm('are you sure?')"><span data-feather="x-circle"></span> Delete</button>
+      </form>
 
       <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top img-fluid mt-4" alt="{{ $post->category->name }}">
 
